d08020b0050339f0fdb5ca3d6437147212fad454
Jay Panchal <panchalj272@gmail.com>
2024-12-09 16:54:18 +0530

    feat: Header component with flow validation

---
+++ src/components/Header.tsx
@@ -0,0 +1,25 @@
+import { CheckBadgeIcon } from "@heroicons/react/24/outline";
+import useFlowValidator from "../hooks/useFlowValidator";
+import { toast } from "react-toastify";
+
+const Header = () => {
+  const [validateFlow] = useFlowValidator((isValid) => {
+    if (isValid) toast.success("Flow is valid");
+    else toast.error("Cannot Save Flow it is invalid");
+  });
+
+  return (
+    <div className="h-12 bg-white border border-b-gray-200 px-5 py-2 flex justify-between">
+      <h1 className="">Chatbot flow builder</h1>
+      <button
+        onClick={validateFlow}
+        className="h-full outline-none flex text-blue-500 items-center justify-center gap-x-2 border border-blue-500 rounded-lg text-sm transition hover:bg-blue-400/15 px-3 mr-5"
+      >
+        <CheckBadgeIcon className="w-5 h-5" />
+        Save Changes
+      </button>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/hooks/useFlowValidator.tsx
@@ -0,0 +1,38 @@
+import { Edge, Node, getConnectedEdges, useReactFlow } from "@xyflow/react";
+
+const findEdges = (node: Node, connectedEdges: Edge[]) => {
+  const outgoingEdges = connectedEdges.filter(
+    (edge) => edge.source === node.id
+  );
+  const incomingEdges = connectedEdges.filter(
+    (edge) => edge.target === node.id
+  );
+  return { outgoingEdges, incomingEdges };
+};
+
+const useFlowValidator = (onValidate?: (isValid: boolean) => void) => {
+  const { getNodes, getEdges } = useReactFlow();
+
+  const validate = () => {
+    const nodes = getNodes();
+    const edges = getEdges();
+
+    if (nodes.length === 1) {
+      onValidate?.(true);
+      return;
+    }
+
+    const connectedEdges = getConnectedEdges(nodes, edges);
+
+    const hasUnconnectedNodes = nodes.some(node => {
+      const { incomingEdges, outgoingEdges } = findEdges(node, connectedEdges);
+      return incomingEdges.length === 0 && outgoingEdges.length === 0;
+    });
+
+    onValidate?.(!hasUnconnectedNodes);
+  };
+
+  return [validate];
+};
+
+export default useFlowValidator;
