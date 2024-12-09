0db628d955af2c86a12cc822f87c41b40291c1d1
Jay Panchal <panchalj272@gmail.com>
2024-12-09 17:07:19 +0530

    feat: Custom text message node

---
+++ src/components/nodes/TextMessageNode.tsx
@@ -0,0 +1,56 @@
+import {
+  Handle,
+  NodeProps,
+  Position,
+  useHandleConnections,
+} from "@xyflow/react";
+import { TextMessageNodeData } from ".";
+import { ChatBubbleOvalLeftIcon } from "@heroicons/react/24/outline";
+import { twMerge } from "tailwind-merge";
+
+type TextMessageNodeProps = NodeProps<TextMessageNodeData>;
+
+const TextMessageNodeRaw = ({
+  isConnectable,
+  selected,
+  data,
+}: TextMessageNodeProps) => {
+  const connections = useHandleConnections({
+    type: "source",
+  });
+
+  return (
+    <>
+      <div
+        className={twMerge(
+          "w-44 overflow-clip border border-blue-100 rounded-lg bg-white shadow-sm backdrop-blur-xl transition divide-y divide-blue-100",
+          selected && "border-blue-400"
+        )}
+      >
+        <div className="bg-green-300/50 px-2 py-1 flex items-center">
+          <ChatBubbleOvalLeftIcon className="size-3" />
+          <span className="ml-1.5 text-xs font-medium leading-none tracking-wide">
+            Send Message
+          </span>
+        </div>
+        {data.message?.trim() === "" ? (
+          <div className="px-2 py-3 italic text-xs">No message yet...</div>
+        ) : (
+          <div className="px-2 py-3 text-xs">{data.message}</div>
+        )}
+      </div>
+      <Handle
+        type="target"
+        position={Position.Left}
+        isConnectable={isConnectable}
+      />
+      <Handle
+        type="source"
+        position={Position.Right}
+        isConnectable={connections.length === 0}
+      />
+    </>
+  );
+};
+
+export default TextMessageNodeRaw;
---
+++ src/components/nodes/index.tsx
@@ -0,0 +1,39 @@
+import type { Node, NodeTypes, BuiltInNode } from "@xyflow/react";
+import TextMessageNode from "./TextMessageNode";
+import { CustomNodes, CustomNodeType } from "../../constants/constants";
+
+export type TextMessageData = {
+  message?: string;
+};
+
+export type TextMessageNodeData = Node<
+  TextMessageData,
+  CustomNodes.TEXT_MESSAGE_NODE
+>;
+
+export type AppNodeData<T extends CustomNodeType> =
+  T extends `${CustomNodes.TEXT_MESSAGE_NODE}`
+    ? TextMessageData
+    : Record<string, any>;
+
+export type AppNode = BuiltInNode | TextMessageNodeData;
+
+export const initialNodes: AppNode[] = [
+  {
+    id: "a",
+    type: CustomNodes.TEXT_MESSAGE_NODE,
+    position: { x: 100, y: 100 },
+    data: { message: "Test Message 1" },
+  },
+  {
+    id: "b",
+    type: CustomNodes.TEXT_MESSAGE_NODE,
+    position: { x: 400, y: -50 },
+    data: { message: "Test Message 2" },
+  },
+];
+
+export const nodeTypes = {
+  [CustomNodes.TEXT_MESSAGE_NODE]: TextMessageNode,
+  // Add any of your custom nodes here!
+} satisfies NodeTypes;
